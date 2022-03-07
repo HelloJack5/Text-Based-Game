639d59ab0a6b548350815072a66547a2f0c86fda
HelloJack5 <jakobkokokosec@gmail.com>
2022-03-07 07:56:23 +0100

    Added start screen

--- Text Based game/main.c
+++ Text Based game/main.c
@@ -1,11 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define BOOL char
-#define FALSE 0
-#define TRUE 1
+#include <stdbool.h>
+
+static char input[100] = "look around";
+
+static bool getInput(void)
+{
+   printf("\nType command: ");
+   return fgets(input, sizeof input, stdin) != NULL;
+}
 
 int main()
 {
-    printf("Hello world!\n");
+    char name[20];
+    printf("Welcome to \"insert game name here\"\n");
+    printf("Please choose a name(up to 20 characters): ");
+    scanf("%s", name);
+    printf("Hello %s!\n", name);
+    printf("You find yourself at a crossroads, in front of you is a big building with a sign,\nto your left there are thick trees with a path beneath\nand to your right there is a playground with kids playing at it.\n");
+    printf("Which way do you wish to go? Type left to go left, right to go right, and straight to go straight.\n");
+    //while (parseAndExecute(input) && getInput());
+    printf("Goodbye, see you later.");
+
+
     return 0;
 }
+
