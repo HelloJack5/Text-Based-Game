8fe4a8e4356fdab30bd5221413b4f9140a5bd0db
HelloJack5 <jakobkokokosec@gmail.com>
2022-03-08 13:32:29 +0100

    Added endless loop

--- Text Based game/main.c
+++ Text Based game/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include "parsexec.h"
 
 static char input[100] = "look around";
 
@@ -17,9 +18,9 @@ int main()
     printf("Please choose a name(up to 20 characters): ");
     scanf("%s", name);
     printf("Hello %s!\n", name);
-    printf("You find yourself at a crossroads, in front of you is a big building with a sign,\nto your left there are thick trees with a path beneath\nand to your right there is a playground with kids playing at it.\n");
-    printf("Which way do you wish to go? Type left to go left, right to go right, and straight to go straight.\n");
-    //while (parseAndExecute(input) && getInput());
+    //printf("You find yourself at a crossroads, in front of you is a big building with a sign,\nto your left there are thick trees with a path beneath\nand to your right there is a playground with kids playing at it.\n");
+    //printf("Which way do you wish to go? Type left to go left, right to go right, and straight to go straight.\n");
+    while (parseAndExecute(input) && getInput());
     printf("Goodbye, see you later.");
 
 
