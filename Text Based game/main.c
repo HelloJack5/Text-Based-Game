ab49072445ca9812b44a65a4779b0c668eca8d68
HelloJack5 <jakobkokokosec@gmail.com>
2022-05-08 19:39:21 +0200

    Added locations

--- Text Based game/main.c
+++ Text Based game/main.c
@@ -18,8 +18,6 @@ int main()
     printf("Please choose a name(up to 20 characters): ");
     scanf("%s", name);
     printf("Hello %s!\n", name);
-    //printf("You find yourself at a crossroads, in front of you is a big building with a sign,\nto your left there are thick trees with a path beneath\nand to your right there is a playground with kids playing at it.\n");
-    //printf("Which way do you wish to go? Type left to go left, right to go right, and straight to go straight.\n");
     while (parseAndExecute(input) && getInput());
     printf("Goodbye, see you later.");
 
--- Text Based game/parsexec.c
+++ Text Based game/parsexec.c
@@ -1,7 +1,7 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
-
+#include "location.h"
 bool parseAndExecute(char *input)
 {
    char *verb = strtok(input, " \n");
@@ -14,11 +14,11 @@ bool parseAndExecute(char *input)
       }
       else if (strcmp(verb, "look") == 0)
       {
-         printf("It is very dark in here.\n");
+         executeLook(noun);
       }
       else if (strcmp(verb, "go") == 0)
       {
-         printf("It's too dark to go anywhere.\n");
+         executeGo(noun);
       }
       else
       {
